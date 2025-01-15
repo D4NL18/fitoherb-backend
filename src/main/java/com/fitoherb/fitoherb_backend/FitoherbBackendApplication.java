bcb2b468a98654938f710eeeaea0ed7d09cb667f
Daniel Marinho <danielmarinho8@hotmail.com>
2025-01-15 13:25:34 -0300

    project creation

---
+++ src/main/java/com/fitoherb/fitoherb_backend/FitoherbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitoherb.fitoherb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitoherbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitoherbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitoherb/fitoherb_backend/FitoherbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitoherb.fitoherb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitoherbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
