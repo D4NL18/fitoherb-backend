34b84b86d8e921b2b4174ca877cdd0654a553847
Daniel Marinho <danielmarinho8@hotmail.com>
2025-02-04 19:23:28 -0300

    fix: product images work with frontend

--- src/main/java/com/fitoherb/fitoherb_backend/controllers/ProductController.java
+++ src/main/java/com/fitoherb/fitoherb_backend/controllers/ProductController.java
@@ -1,13 +1,17 @@
 package com.fitoherb.fitoherb_backend.controllers;
 
 import com.fitoherb.fitoherb_backend.dtos.ProductRecordDto;
+import com.fitoherb.fitoherb_backend.dtos.SupplierRecordDto;
+import com.fitoherb.fitoherb_backend.models.CategoryModel;
 import com.fitoherb.fitoherb_backend.models.ProductModel;
+import com.fitoherb.fitoherb_backend.models.SupplierModel;
 import com.fitoherb.fitoherb_backend.services.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,7 +24,14 @@ public class ProductController {
     ProductService productService;
 
     @PostMapping("/products")
-    public ResponseEntity<Object> postProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+    public ResponseEntity<Object> addProduct(@RequestParam("productName") String productName,
+                                              @RequestParam("productImageUrl") MultipartFile productImageUrl,
+                                              @RequestParam("price_in_cents") int price_in_cents,
+                                              @RequestParam("productDescription") String productDescription,
+                                              @RequestParam("productCategory") CategoryModel productCategory,
+                                              @RequestParam("supplier") SupplierModel supplier) {
+        ProductRecordDto productRecordDto = new ProductRecordDto(productName, price_in_cents, productDescription, productCategory.getIdCategory().toString(),productImageUrl, supplier.getSupplierId().toString());
+
         return productService.postProduct(productRecordDto);
     }
 
--- src/main/java/com/fitoherb/fitoherb_backend/services/ProductService.java
+++ src/main/java/com/fitoherb/fitoherb_backend/services/ProductService.java
@@ -9,16 +9,25 @@ import com.fitoherb.fitoherb_backend.repositories.ProductRepository;
 import com.fitoherb.fitoherb_backend.repositories.SupplierRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class ProductService {
+
+    @Value("${path.productImages}")
+    private String uploadDir;
 
     @Autowired
     ProductRepository productRepository;
@@ -31,31 +40,65 @@ public class ProductService {
 
     public ResponseEntity<Object> postProduct(ProductRecordDto productRecordDto) {
 
+        MultipartFile image = productRecordDto.productImageUrl();
+        // Verifica se a categoria existe
         Optional<CategoryModel> category = categoryRepository.findById(UUID.fromString(productRecordDto.productCategory()));
         if (category.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found");
         }
 
+        // Verifica se o fornecedor existe
         Optional<SupplierModel> supplier = supplierRepository.findById(UUID.fromString(productRecordDto.supplier()));
         if (supplier.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Supplier not found");
         }
 
-        Optional<ProductModel> product = productRepository.findByProductName(productRecordDto.productName());
-        if(!product.isEmpty()) {
+        // Verifica se o produto já existe
+        Optional<ProductModel> existingProduct = productRepository.findByProductName(productRecordDto.productName());
+        if (existingProduct.isPresent()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Product already exists");
         }
 
-        var productModel = new ProductModel();
-        productModel.setProductName(productRecordDto.productName());
-        productModel.setPrice_in_cents(productRecordDto.price_in_cents());
-        productModel.setProductDescription(productRecordDto.productDescription());
-        productModel.setProductImageUrl(productRecordDto.productImageUrl());
-        productModel.setProductCategory(category.get());
-        productModel.setSupplier(supplier.get());
+        if (image.isEmpty()) {
+            return ResponseEntity.badRequest().body("Image file is required.");
+        }
 
-        System.out.println(productModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+        String fileName = image.getOriginalFilename();
+        String fileExtension = fileName.substring(fileName.lastIndexOf('.'));  // Extensão da imagem (ex: .png, .jpg)
+
+        // Gerar uma chave aleatória (UUID) e anexar ao nome do arquivo
+        String uniqueFileName = UUID.randomUUID().toString() + fileName + fileExtension;
+
+        // Caminho do arquivo completo para salvar a imagem
+        String filePath = this.uploadDir + File.separator + uniqueFileName;
+
+        // Caminho relativo para salvar no banco de dados
+        String localPath = "images/productImages/" + uniqueFileName;
+
+        try {
+
+            File dir = new File(uploadDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            File serverFile = new File(filePath);
+            image.transferTo(serverFile);
+
+            ProductModel productModel = new ProductModel();
+            productModel.setProductName(productRecordDto.productName());
+            productModel.setProductCategory(category.get());
+            productModel.setProductDescription(productRecordDto.productDescription());
+            productModel.setSupplier(supplier.get());
+            productModel.setPrice_in_cents(productRecordDto.price_in_cents());
+            productModel.setProductImageUrl(localPath);
+
+            productRepository.save(productModel);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(productModel);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Error saving file: " + e.getMessage());
+        }
     }
 
 
