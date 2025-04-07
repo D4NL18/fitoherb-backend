9084b85f1be9ffcd71d4d974b24b33a39e832ff5
Daniel Marinho <danielmarinho8@hotmail.com>
2025-04-07 20:24:52 -0300

    Update Cors

--- src/main/java/com/fitoherb/fitoherb_backend/infra/cors/CorsConfig.java
+++ src/main/java/com/fitoherb/fitoherb_backend/infra/cors/CorsConfig.java
@@ -10,8 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://fitoherb.vercel.app")
+                .allowedOrigins("http://localhost:4200", "https://fitoherb.vercel.app")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
